a56f12f39a66eaacf6a902da8f71c30b01401f2a
Kintakayohei <kintaka.youhei.24u@st.kyoto-u.ac.jp>
2025-03-19 00:30:58 +0900

    Reinitialize repository

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,2 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
 import Raty from "./raty";
 window.Raty = Raty
-//= require jquery
-//= require jquery_ujs
-import jQuery from "jquery";
-window.$ = window.jQuery = jQuery;
